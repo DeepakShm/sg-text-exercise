7a6bd85e54731d1823770a3ec15b1bc861c77e55
deepak_shm <deepak.sharma@axeno.co>
2024-12-05 21:52:48 +0530

    bug-fix: resolved scrolltrigger issue

--- src/hooks/useSectionIndicator.ts
+++ src/hooks/useSectionIndicator.ts
@@ -1,15 +1,14 @@
 import { useLayoutEffect } from "react";
 import gsap from "gsap";
-import { ScrollToPlugin, ScrollTrigger } from "gsap/all";
+import { ScrollTrigger } from "gsap/all";
 
-gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
+gsap.registerPlugin(ScrollTrigger);
 
 function useSectionIndicator(container: React.RefObject<HTMLDivElement>) {
   useLayoutEffect(() => {
     const context = gsap.context(() => {
       const sectionCount = document.getElementById("slide-count");
       const links: HTMLAnchorElement[] = gsap.utils.toArray(".nav-links a");
-
       // creating scroll-trigger for each section to check intersection on scroll
       links.forEach((a, idx) => {
         const href = a.getAttribute("href");
@@ -35,12 +34,9 @@ function useSectionIndicator(container: React.RefObject<HTMLDivElement>) {
         sectionCount.style.top = `${22 * idx}px`;
         sectionCount.innerText = `0${idx + 1}`;
       }
-    }, container);
+    });
 
-    return () => {
-      // cleanup on unmount
-      context.revert();
-    };
+    return () => context.revert();
   }, [container]);
 }
 
